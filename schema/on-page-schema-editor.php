a52222832f61b81394f8cdac2f0d3dc594c07fa0
Lojikell <132538867+Lojikell@users.noreply.github.com>
2025-03-12 13:01:36 +1000

    Updated unicode for special characters for custom schema (#8)
    * Updated unicode for special characters
    
    * Wrapped in script tags

--- schema/on-page-schema-editor.php
+++ schema/on-page-schema-editor.php
@@ -176,7 +176,9 @@ function zynith_seo_output_custom_schema() {
             
             // Output schema with comment tags around the script
             echo "\n<!-- Custom Schema -->\n";
-            echo '<script type="application/ld+json">' . $schema_data . '</script>';
+            echo '<script type="application/ld+json">';
+            echo wp_json_encode( $schema, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+            echo '</script>';
             echo "\n<!-- End Custom Schema -->\n";
         }
     }
